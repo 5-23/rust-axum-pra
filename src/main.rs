4d78c383badc18b70febe10ffec7c540d87cf789
5-23 <86705803+5-23@users.noreply.github.com>
2023-02-14 14:30:31 +0900

    return json

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 mod cnt_db;
-use axum::{*, routing::get, response::Html};
+use axum::{*, routing::{get, post}, response::Html};
+use serde::{Deserialize, Serialize};
+#[derive(Serialize)]
+struct Js{
+    name: &'static str,
+    info: Info
+}
+#[derive(Serialize)]
+struct Info{
+    mail: &'static str,
+    tell: (u16, u16, u16),
+    star: usize
+}
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/{name}", get(index))
         .route("/up", get(count_up))
+        .route("/info", get(json))
         ;
 
     Server::bind(&"127.0.0.1:8080".parse().unwrap())
@@ -22,4 +35,15 @@ async fn index(name: String) -> Html<&'static str>{
 async fn count_up() -> &'static str{
     let n = cnt_db::db::Db::new();
     "ok"
+}
+async fn json() -> (http::StatusCode, Json<Js>){
+    let j = Json::from(Js{
+        name: "5-23",
+        info: Info {
+            mail: "yhanbyeol6@gmail.com",
+            tell: (010, 1234, 5678),
+            star: 0
+        }
+    });
+    (http::StatusCode::CREATED, j)
 }
